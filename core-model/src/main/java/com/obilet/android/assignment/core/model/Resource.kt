a7d637d88ad8b1aeacb3589feb47b1b6415ca7fd
bturkali <berkan.turkali@path.com.tr>
2024-04-08 22:19:27 +0300

    add Resource & UiText utility classes

---
+++ core-model/src/main/java/com/obilet/android/assignment/core/model/Resource.kt
@@ -0,0 +1,14 @@
+package com.obilet.android.assignment.core.model
+
+sealed class Resource<T>(
+    val data: T? = null,
+    val error: UiText? = null,
+    val errorCode: Int? = null
+) {
+    class Success<T>(data: T? = null) : Resource<T>(data)
+
+    class Error<T>(code: Int? = null, text: UiText, data: T? = null) :
+        Resource<T>(data = data, errorCode = code, error = text)
+
+    class Loading<T>(data: T? = null) : Resource<T>(data)
+}
